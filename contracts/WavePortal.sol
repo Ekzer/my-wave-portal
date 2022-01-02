5f89ba1ec7f6431247335521b2dfc1cdaa0fc62b
jvives <julian.vives@canal-plus.com>
2022-01-02 03:34:23 +0100

    NewWave Event, getAllWaves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,14 +7,29 @@ contract WavePortal {
     uint256 totalWaves;
     mapping(address => uint256) mapWaver;
 
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+    event NewWave(address _sender, uint256 _timestamp, string _message);
+
+    Wave[] waves;
     constructor(){
         console.log("Hey, I'm a smart contract and I am deployed!");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
         mapWaver[msg.sender] += 1;
-        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getWavesByAddr(address id) public view returns (uint256) {
--- test/wave-test.js
+++ test/wave-test.js
@@ -1,10 +1,12 @@
 const {expect} = require("chai");
 
 describe("WavePortal Contract", () => {
-    it("Wave total count should increase and be linked to msg.send", async () => {
+    let waveContract;
+    before(async() => {
         const wavePortalContract = await hre.ethers.getContractFactory("WavePortal");
-        const waveContract = await wavePortalContract.deploy();
-
+        waveContract = await wavePortalContract.deploy();
+    })
+    it("Wave total count should increase and be linked to msg.send", async () => {
         const [owner] = await hre.ethers.getSigners();
         console.log("WavePortal Smart Contract deployed to :" + waveContract.address);
         console.log("WavePortal Smart Contract deployed by :" + owner.address);
@@ -12,7 +14,8 @@ describe("WavePortal Contract", () => {
         let totalWaves = await waveContract.getTotalWaves();
         expect(totalWaves).to.equal(0);
 
-        const waveTxn = await waveContract.wave();
+        const waveTxn = await waveContract.wave("Hello World");
+        await waveTxn.wait();
         totalWaves = await waveContract.getTotalWaves();
         expect(totalWaves).to.equal(1);
 
@@ -20,17 +23,27 @@ describe("WavePortal Contract", () => {
         expect(ownerWaves).to.equal(1);
     })
 
-    it("Wave from other people should be stored", async() => {
-        const wavePortalContract = await hre.ethers.getContractFactory("WavePortal");
-        const waveContract = await wavePortalContract.deploy();
-
-        const [owner, randomPerson] = await hre.ethers.getSigners();
-
-        const waveRandomPersonTxn = await waveContract.connect(randomPerson).wave();
+    it("Wave from other people should be stored and emit newWave event", async() => {
+        const [_, randomPerson] = await hre.ethers.getSigners();
+        const message = "Hello World 2";
+        const waveRandomPersonTxn = await waveContract.connect(randomPerson).wave(message);
+        const receipt = await waveRandomPersonTxn.wait();
+        const newWaveEvent = receipt.events.find(event => event.event === "NewWave");
+        expect(newWaveEvent).to.exist;
+        const [from, timestamp, msg] = newWaveEvent.args;
+        expect(from).to.eql(randomPerson.address);
+        expect(msg).to.eql(message);
         const totalWaves = await waveContract.getTotalWaves();
-        expect(totalWaves).to.equal(1);
+        expect(totalWaves).to.equal(2);
 
         const randomPersonWaves = await waveContract.getWavesByAddr(randomPerson.address);
         expect(randomPersonWaves).to.equal(1);
+    })
+
+    it("Get every wave object", async() => {
+        const waves = await waveContract.getAllWaves();
+        console.log(waves);
+        expect(waves.length).to.equal(2);
+        expect(waves.map(elem => elem.message)).to.eql(["Hello World", "Hello World 2"]);
     })
 })
