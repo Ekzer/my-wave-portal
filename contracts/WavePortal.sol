e6d7e0680c934b1d5c9322fd9cf8bdc179adeadd
jvives <julian.vives@canal-plus.com>
2022-01-04 01:23:49 +0100

    rewards to waver by smartContract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -15,17 +15,22 @@ contract WavePortal {
     event NewWave(address _sender, uint256 _timestamp, string _message);
 
     Wave[] waves;
-    constructor(){
+    constructor() payable {
         console.log("Hey, I'm a smart contract and I am deployed!");
     }
 
     function wave(string memory _message) public {
         totalWaves += 1;
         mapWaver[msg.sender] += 1;
-
+        
         waves.push(Wave(msg.sender, _message, block.timestamp));
-
         emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(prizeAmount <= address(this).balance, "No more enough funds in contract to give prize");
+        (bool success, ) = msg.sender.call{value: prizeAmount}("");
+        console.log(success);
+        require(success, "Failed to withdraw money from contract.");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,9 @@ const main = async() => {
     const [owner] = await hre.ethers.getSigners();
     const balance = await owner.getBalance();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // get contract
-    const waveContract = await waveContractFactory.deploy(); // create a local ethereum network, fresh blockchain, deploy on it
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1")
+    }); // create a local ethereum network, fresh blockchain, deploy on it
     await waveContract.deployed(); //
     console.log("WavePortal Smart Contract deployed to :" + waveContract.address);
     console.log("WavePortal Smart Contract deployed by :" + owner.address);
